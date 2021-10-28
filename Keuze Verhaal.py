f04aa230dbf5e33cd4752aec30292d5cd6949fe6
Jahvairo <33427@ma-web.nl>
2021-10-28 10:51:17 +0200

    9TH Commit

--- Keuze Verhaal.py
+++ Keuze Verhaal.py
@@ -1,5 +1,12 @@
+
+import time
+
 print("Hello You!")
 print("Wie ben jij?")
 naam = input ()
-print("Hello" , naam)
-print("Dit verhaal is gebaseerd op waargebeurde verhalen van vluchtelingen die naar Nederland zijn gevlucht.")
+print("Hallo," , naam)
+time.sleep(2)
+print("Dit verhaal is gebaseerd op een waargebeurd verhaal van Parwana.")
+time.sleep(2)
+print("Ze is als 8-jarige meisje met haar vader uit Afghanistan gevlucht vanwege een dreigbrief van de Taliban.")
+#inleiding ^ 
